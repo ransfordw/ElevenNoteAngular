a345628957e8d451d8756fd60ccc0aae7a375cf7
Ransford Walker <ransfordw@gmail.com>
2019-06-03 16:56:58 -0400

    Updates handling vulnerabilities

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,4 +14,9 @@ export class AuthService {
   register(regUserData: RegisterUser) {
     return this._http.post(`${Api_Url}/api/Account/Register`, regUserData);
   }
+
+  login(loginInfo){
+    const str = `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
+    return this._http.post(`${Api_Url}/token`, str);
+  }
 }
